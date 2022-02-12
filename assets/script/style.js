839e6a83629be31edcd4e71f30f1010299c25443
Haybuka <paschal.nduleo@gmail.com>
2022-02-13 04:45:23 +0800

    added resume links

--- assets/script/style.js
+++ assets/script/style.js
@@ -2,3 +2,10 @@ let typed = new Typed('#newString', {
     stringsElement: '.new-strings'
   });
 
+let harmburger = document.querySelector('.harmburger');
+let navClick = document.querySelector('.nav-click');
+let onHarmbugerClick = document.querySelector('.onHarmbugerClick');
+harmburger.addEventListener('click',()=>{
+    onHarmbugerClick.classList.toggle('hide')
+    navClick.classList.toggle('display')
+})
