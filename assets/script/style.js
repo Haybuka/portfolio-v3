f89c18ea46ed40bef4ebbabcc3b24401e7fc77a9
Haybuka <paschal.nduleo@gmail.com>
2022-05-15 06:55:09 +0800

    FEATURE : Add Toggle type

--- assets/script/style.js
+++ assets/script/style.js
@@ -4,8 +4,24 @@ let typed = new Typed('#newString', {
 
 let harmburger = document.querySelector('.harmburger');
 let navClick = document.querySelector('.nav-click');
+let projectType = document.querySelectorAll('.card-group .card');
+let vanilla = document.querySelector('#vanilla');
+let react = document.querySelector('#react');
 let onHarmbugerClick = document.querySelector('.onHarmbugerClick');
+
+
 harmburger.addEventListener('click',()=>{
     onHarmbugerClick.classList.toggle('hide')
     navClick.classList.toggle('display')
+})
+
+vanilla.addEventListener('click',function(){
+  this.classList.add('active')
+  react.classList.remove('active')
+})
+
+react.addEventListener('click',function(){
+  this.classList.add('active')
+  vanilla.classList.remove('active')
+
 })
