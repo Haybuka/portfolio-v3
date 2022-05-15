33b528b7953438df751dc8b3ee8672ee68ed4847
Haybuka <paschal.nduleo@gmail.com>
2022-05-15 08:25:51 +0800

    FEATURE : Add Js that handles toggle of both react & vanilla

--- assets/script/style.js
+++ assets/script/style.js
@@ -7,6 +7,8 @@ let navClick = document.querySelector('.nav-click');
 let projectType = document.querySelectorAll('.card-group .card');
 let vanilla = document.querySelector('#vanilla');
 let react = document.querySelector('#react');
+let reactGroup = document.querySelector('#react-group')
+let vanillaGroup = document.querySelector('#vanilla-group')
 let onHarmbugerClick = document.querySelector('.onHarmbugerClick');
 
 
@@ -18,10 +20,13 @@ harmburger.addEventListener('click',()=>{
 vanilla.addEventListener('click',function(){
   this.classList.add('active')
   react.classList.remove('active')
+  reactGroup.classList.add('d-none')
+  vanillaGroup.classList.remove('d-none')
 })
 
 react.addEventListener('click',function(){
   this.classList.add('active')
   vanilla.classList.remove('active')
-
+  vanillaGroup.classList.add('d-none')
+  reactGroup.classList.remove('d-none')
 })
