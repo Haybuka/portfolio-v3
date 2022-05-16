96e78848ebd449d5f5c301a6f5054203611bba1d
Haybuka <paschal.nduleo@gmail.com>
2022-05-16 08:46:06 +0800

    Feature : Add preloader,with a delay of 1.5s

--- assets/script/style.js
+++ assets/script/style.js
@@ -10,7 +10,7 @@ let react = document.querySelector('#react');
 let reactGroup = document.querySelector('#react-group')
 let vanillaGroup = document.querySelector('#vanilla-group')
 let onHarmbugerClick = document.querySelector('.onHarmbugerClick');
-
+const loader = document.querySelector('.loader')
 
 harmburger.addEventListener('click',()=>{
     onHarmbugerClick.classList.toggle('hide')
@@ -21,12 +21,23 @@ vanilla.addEventListener('click',function(){
   this.classList.add('active')
   react.classList.remove('active')
   reactGroup.classList.add('d-none')
+  loader.classList.remove('d-none')
+  setTimeout(() => {
   vanillaGroup.classList.remove('d-none')
+  loader.classList.add('d-none')
+
+  }, 2000);
 })
 
 react.addEventListener('click',function(){
   this.classList.add('active')
   vanilla.classList.remove('active')
   vanillaGroup.classList.add('d-none')
-  reactGroup.classList.remove('d-none')
+  loader.classList.remove('d-none')
+
+  setTimeout(() => {
+   reactGroup.classList.remove('d-none')
+  loader.classList.add('d-none')
+
+  }, 2000);
 })
