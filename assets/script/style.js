3301950a0693e52251638f1f91a25df154022a45
Haybuka <paschal.nduleo@gmail.com>
2022-05-16 09:16:26 +0800

    Update : made the preloader more custom

--- assets/script/style.js
+++ assets/script/style.js
@@ -1,7 +1,4 @@
-let typed = new Typed('#newString', {
-    stringsElement: '.new-strings'
-  });
-
+//Selection of query elements
 let harmburger = document.querySelector('.harmburger');
 let navClick = document.querySelector('.nav-click');
 let projectType = document.querySelectorAll('.card-group .card');
@@ -12,32 +9,47 @@ let vanillaGroup = document.querySelector('#vanilla-group')
 let onHarmbugerClick = document.querySelector('.onHarmbugerClick');
 const loader = document.querySelector('.loader')
 
+
+//TypeJs usage after linked
+let typed = new Typed('#newString', {
+    stringsElement: '.new-strings'
+  });
+
+//Harmburger toggle select
 harmburger.addEventListener('click',()=>{
     onHarmbugerClick.classList.toggle('hide')
     navClick.classList.toggle('display')
 })
 
+//click event to toggle modes between react & vanilla
 vanilla.addEventListener('click',function(){
+
+   //class changes and toggle
   this.classList.add('active')
   react.classList.remove('active')
   reactGroup.classList.add('d-none')
   loader.classList.remove('d-none')
-  setTimeout(() => {
-  vanillaGroup.classList.remove('d-none')
-  loader.classList.add('d-none')
 
-  }, 2000);
+  // Preloader usage and 1s time
+  setTimeout(() => {
+     vanillaGroup.classList.remove('d-none')
+     loader.classList.add('d-none')
+  }, 1000);
 })
 
 react.addEventListener('click',function(){
+
+   //class changes and toggle
   this.classList.add('active')
   vanilla.classList.remove('active')
   vanillaGroup.classList.add('d-none')
   loader.classList.remove('d-none')
 
+  // Preloader usage and 1s time
   setTimeout(() => {
-   reactGroup.classList.remove('d-none')
-  loader.classList.add('d-none')
-
-  }, 2000);
+     reactGroup.classList.remove('d-none')
+     loader.classList.add('d-none')
+  }, 1000);
 })
+
+//end of toggle click event
